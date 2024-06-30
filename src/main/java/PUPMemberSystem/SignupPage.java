6756111c21fb5c7c778db7c9e706473c05a23755
selldadada <crisellesayo6@gmail.com>
2024-07-01 00:11:55 +0800

    Criselle Sayo

--- src/main/java/PUPMemberSystem/SignupPage.java
+++ src/main/java/PUPMemberSystem/SignupPage.java
@@ -8,9 +8,10 @@ import java.awt.Font;
 
 public class SignupPage extends JFrame implements ActionListener {
 
-    private JLabel lblbg, lbltitle, lblDescription, lblLName, lblFName, lblMName, lblStudNo, lblCourse, lblYear, lblAddress;
+    private JLabel lblbg, lbltitle, lblDescription, lblLName, lblFName, lblMName, lblStudNo, lblCourse, lblYear, lblAddress, lblContactNo, lblBday, lblPosition, lblAffiliation, lblEmail, lblPassword;
     private JPanel pnlYellow;
-    private JTextField txfLName, txfFName, txfMName, txfStudNo, txfCourse, txfYear, txfAddress;
+    private JTextField txfLName, txfFName, txfMName, txfStudNo, txfCourse, txfYear, txfAddress, txfContactNo, txfBday, txfPosition, txfAffiliation, txfEmail;
+    private JPasswordField pwfPassword;
     private JButton btnMenu, btnSignup;
 
     SignupPage() {
@@ -43,73 +44,141 @@ public class SignupPage extends JFrame implements ActionListener {
         pnlYellow.setBounds(130, 120, 730, 370);
         lblbg.add(pnlYellow);
 
-        // Please fill in every field text
+        // Please fill in every field
         lblDescription = new JLabel("Please fill in every field.");
-        lblDescription.setBounds(15, 5, 200, 30);
-        lblDescription.setFont(new Font("Aptos", Font.PLAIN, 13));
+        lblDescription.setBounds(15, 7, 200, 30);
+        lblDescription.setFont(new Font("Aptos", Font.PLAIN, 14));
         pnlYellow.add(lblDescription);
         
         // Last Name
         lblLName = new JLabel("Last Name");
-        lblLName.setBounds(35, 40, 150, 40);
+        lblLName.setBounds(35, 45, 150, 40);
         lblLName.setFont(new Font("Aptos", Font.BOLD, 14));
         pnlYellow.add(lblLName);
         
         txfLName = new JTextField();
-        txfLName.setBounds(35,75,200,30);
+        txfLName.setBounds(35,80,200,30);
         pnlYellow.add(txfLName);
         
         // First Name
         lblFName = new JLabel("First Name");
-        lblFName.setBounds(265, 40, 150, 40);
+        lblFName.setBounds(265, 45, 150, 40);
         lblFName.setFont(new Font("Aptos", Font.BOLD, 14));
         pnlYellow.add(lblFName);
         
         txfFName = new JTextField();
-        txfFName.setBounds(265,75,200,30);
+        txfFName.setBounds(265,80,200,30);
         pnlYellow.add(txfFName);
         
         // Middle Name
         lblMName = new JLabel("Middle Name");
-        lblMName.setBounds(495, 40, 150, 40);
+        lblMName.setBounds(495, 45, 150, 40);
         lblMName.setFont(new Font("Aptos", Font.BOLD, 14));
         pnlYellow.add(lblMName);
         
         txfMName = new JTextField();
-        txfMName.setBounds(495,75,200,30);
+        txfMName.setBounds(495,80,200,30);
         pnlYellow.add(txfMName);
         
         //Student Number
         lblStudNo = new JLabel("Student Number");
-        lblStudNo.setBounds(35, 110, 150, 40);
+        lblStudNo.setBounds(35, 115, 150, 40);
         lblStudNo.setFont(new Font("Aptos", Font.BOLD, 14));
         pnlYellow.add(lblStudNo);
         
         txfStudNo = new JTextField();
-        txfStudNo.setBounds(35,145,200,30);
+        txfStudNo.setBounds(35,150,200,30);
         pnlYellow.add(txfStudNo);
         
         //Course
         lblCourse = new JLabel("Course");
-        lblCourse.setBounds(265, 110, 100, 40);
+        lblCourse.setBounds(265, 115, 100, 40);
         lblCourse.setFont(new Font("Aptos", Font.BOLD, 14));
         pnlYellow.add(lblCourse);
         
         txfCourse = new JTextField();
-        txfCourse.setBounds(265,145,90,30);
+        txfCourse.setBounds(265,150,90,30);
         pnlYellow.add(txfCourse);
         
         //Year
+        lblYear = new JLabel("Year");
+        lblYear.setBounds(375, 115, 100, 40);
+        lblYear.setFont(new Font("Aptos", Font.BOLD, 14));
+        pnlYellow.add(lblYear);
+        
+        txfYear = new JTextField();
+        txfYear.setBounds(375,150,90,30);
+        pnlYellow.add(txfYear);
         
         //Address
         lblAddress = new JLabel("Address");
-        lblAddress.setBounds(495, 110, 150, 40);
+        lblAddress.setBounds(495, 115, 150, 40);
         lblAddress.setFont(new Font("Aptos", Font.BOLD, 14));
         pnlYellow.add(lblAddress);
         
         txfAddress = new JTextField();
-        txfAddress.setBounds(495,145,200,30);
+        txfAddress.setBounds(495,150,200,30);
         pnlYellow.add(txfAddress);
+        
+        //Contact Number
+        lblContactNo = new JLabel("Contact Number");
+        lblContactNo.setBounds(35, 185, 150, 40);
+        lblContactNo.setFont(new Font("Aptos", Font.BOLD, 14));
+        pnlYellow.add(lblContactNo);
+        
+        txfContactNo = new JTextField();
+        txfContactNo.setBounds(35,220,200,30);
+        pnlYellow.add(txfContactNo);
+        
+        //Birthday
+        lblBday = new JLabel("Birthday");
+        lblBday.setBounds(265, 185, 150, 40);
+        lblBday.setFont(new Font("Aptos", Font.BOLD, 14));
+        pnlYellow.add(lblBday);
+        
+        txfBday = new JTextField();
+        txfBday.setBounds(265,220,200,30);
+        pnlYellow.add(txfBday);
+        
+        //Position
+        lblPosition = new JLabel("Position");
+        lblPosition.setBounds(495, 185, 150, 40);
+        lblPosition.setFont(new Font("Aptos", Font.BOLD, 14));
+        pnlYellow.add(lblPosition);
+
+        txfPosition = new JTextField();
+        txfPosition.setBounds(495,220,200,30);
+        pnlYellow.add(txfPosition);
+        
+        //Affiliation
+        lblAffiliation = new JLabel("Affiliation");
+        lblAffiliation.setBounds(35, 255, 150, 40);
+        lblAffiliation.setFont(new Font("Aptos", Font.BOLD, 14));
+        pnlYellow.add(lblAffiliation);
+        
+        txfAffiliation = new JTextField();
+        txfAffiliation.setBounds(35,285,200,30);
+        pnlYellow.add(txfAffiliation);
+        
+        //Email
+        lblEmail = new JLabel("Email");
+        lblEmail.setBounds(265, 255, 150, 40);
+        lblEmail.setFont(new Font("Aptos", Font.BOLD, 14));
+        pnlYellow.add(lblEmail);
+        
+        txfEmail = new JTextField();
+        txfEmail.setBounds(265,285,200,30);
+        pnlYellow.add(txfEmail);
+        
+        //Password
+        lblPassword = new JLabel("Password");
+        lblPassword.setBounds(495, 255, 150, 40);
+        lblPassword.setFont(new Font("Aptos", Font.BOLD, 14));
+        pnlYellow.add(lblPassword);
+
+        pwfPassword = new JPasswordField();
+        pwfPassword.setBounds(495,290,200,30);
+        pnlYellow.add(pwfPassword);
         
         // button to return to main menu
         btnMenu = new JButton("Main Menu");
